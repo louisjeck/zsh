3664643685bf0daf790c25216894875bcab25ab5
Louis Jeckel <louis.jeckel@outlook.com>
2021-09-23 12:22:29 +0000

    Update install.sh

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+apt install zsh
+
+wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+sh install.sh
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+
+
+cp .zshrc ~
+
+
