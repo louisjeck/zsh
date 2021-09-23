f512c470a0f137097e1adea13dbf0b9d1b6b89f9
Louis Jeckel <louis.jeckel@outlook.com>
2021-09-23 12:48:02 +0000

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -9,7 +9,8 @@ cp .p10k.zsh ~
 
 
 wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
-sh install.sh
+sh install.sh --unattended
+
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
@@ -18,3 +19,4 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 
+chsh -s $(which zsh)
